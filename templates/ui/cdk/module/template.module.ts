c2cacf8bc01d21ee655ea06499ec6957ca99776a
Jerry Orta <jerryorta.dev@gmail.com>
2017-11-12 13:24:34 -0600

    docs(license):

--- templates/ui/cdk/module/template.module.ts
+++ templates/ui/cdk/module/template.module.ts
@@ -1,9 +1,6 @@
 /**
  * @license
- * Copyright Google LLC All Rights Reserved.
- *
- * Use of this source code is governed by an MIT-style license that can be
- * found in the LICENSE file at https://angular.io/license
+ * Copyright SunPower Corporation All Rights Reserved.
  */
 
 import {NgModule} from '@angular/core';
